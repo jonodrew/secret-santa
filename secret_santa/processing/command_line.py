ce4734b2708eff2b620caba888cbe669933a608b
jonathan <jonodrew@users.noreply.github.com>
2024-12-16 20:24:02 +0000

    Add script for sending a reminder
    Signed-off-by: jonathan <jonodrew@users.noreply.github.com>

--- secret_santa/processing/command_line.py
+++ secret_santa/processing/command_line.py
@@ -1,8 +1,10 @@
+import csv
+
 import click
 from twilio.rest import Client
 
 from secret_santa.processing.run import generate_matches
-from send import send
+from send import send, reminder
 
 
 @click.command()
@@ -16,4 +18,18 @@ def secret_santa(year: int):
         if click.confirm("Definitely sending these, right?", abort=True):
             for match in matches:
                 send(match.number, match.name, match.this_year, client)
+        click.echo("Secret! Santa!")
+
+
+@click.command
+@click.argument("year")
+def reminder(year: int):
+    if click.confirm("Do you want to send these pairings?", abort=True):
+        account_sid = click.prompt("Enter Twilio SID")
+        auth_token = click.prompt("Enter Twilio auth token", hide_input=True)
+        client = Client(account_sid, auth_token)
+        if click.confirm("Definitely sending these, right?", abort=True):
+            with open(f"{year}-participants.csv", "r") as people:
+                for person in csv.DictReader(people):
+                    reminder(person.get("number"), person.get("name"), client)
         click.echo("Secret! Santa!")
